d6a0669b350e710ba67980b4a8bcbe88a40374c4
Lcoder <linelijun@163.com>
2018-01-26 15:54:32 +0800

    update

--- constructor.js
+++ constructor.js
@@ -11,3 +11,15 @@ const person2 = new Person("Flash", 25, "SuperHero");
 
 person1.sayPerson();
 person2.sayPerson();
+
+console.log(person1.constructor == Person); //true
+console.log(person2.constructor == Person); //true
+
+console.log(person1 instanceof Object); //true
+console.log(person1 instanceof Person); //true
+console.log(person2 instanceof Object); //true
+console.log(person2 instanceof Person); //true
+
+const o = new Object();
+Person.call(o, "superMan", 28, "manHero");
+o.sayPerson(); //Name:superMan Age:28 Job:manHero
